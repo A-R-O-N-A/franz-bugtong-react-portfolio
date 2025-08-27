6cac0983344a9c3c9744e6a9e75b3f8bbae89d2d
franz <franzbugtong0411@gmail.com>
2025-08-27 14:59:46 +0800

    eddited under constructio nmessage

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -17,8 +17,7 @@ function Main() {
             <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Franz R. Bugtong</h1>
-          <small>(site under construction)</small>
-          <p>Full Stack Engineer</p>
+          <p>Full Stack Engineer (site under construction) </p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
